ac493a7b04565329d9291f769c449ecc11ad1b44
Akuo <janegle@gmail.com>
2017-04-13 11:28:30 +0800

    webMagic seleniumDownloader

--- crawler/src/main/java/me/akuo/crawler/webmagic/BaiduZhidaoPageProcessor.java
+++ crawler/src/main/java/me/akuo/crawler/webmagic/BaiduZhidaoPageProcessor.java
@@ -3,36 +3,43 @@ package me.akuo.crawler.webmagic;
 /**
  * Created by Akuo on 2017/4/12.
  */
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.io.IOException;
 
 public class BaiduZhidaoPageProcessor implements PageProcessor {
 
     private Site site = Site.me().setRetryTimes(3).setSleepTime(100)
             .setUserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36")
             .setUseGzip(true)
-            .addHeader("Host","zhidao.baidu.com")
-            .addHeader("Accept-Encoding","gzip")
-            .addHeader("Accept-Language","zh-CN,zh;q=0.8");
-    private AtomicLong atomicLong = new AtomicLong(0L);
-    private AtomicLong skipNum = new AtomicLong(0L);
+            .addHeader("Host", "zhidao.baidu.com")
+            .addHeader("Accept-Encoding", "gzip")
+            .addHeader("Accept-Language", "zh-CN,zh;q=0.8");
 
     public void process(Page page) {
         page.addTargetRequests(page.getHtml().links().regex("(https://zhidao\\.baidu\\.com/question/[0-9]{19}\\.html.*$)").all());
-        site.addHeader("Referer", page.getUrl().toString());
+
         String answerId = page.getHtml().xpath("//div[@class='wgt-best']/@id").toString();
-        if(answerId != null && answerId.startsWith("best-answer-")){
-            page.putField("title", page.getHtml().xpath("//span[@class='ask-title']/text()").toString());
-            answerId = answerId.replace("best-","");
-            page.putField("answer", page.getHtml().xpath("//div[@id='"+answerId+"']/div[@class='content']/html()").toString());
-            atomicLong.addAndGet(1L);
-        }else {
+        if (answerId != null && answerId.startsWith("best-answer-")) {
+            System.out.println(page.getHtml().xpath("//span[@class='ask-title']/text()").toString());
+            answerId = answerId.replace("best-", "");
+            System.out.println(page.getHtml().xpath("//div[@id='" + answerId + "']/div[@class='content']/html()").toString());
+
+            /* synchronized (this){
+                driver.get(page.getUrl().toString());
+                WebElement bestDiv = driver.findElement(By.className("wgt-best"));
+                String id = bestDiv.getAttribute("id");
+                WebElement content = driver.findElement(By.id("best-content-" + id.replace("best-answer-", "")));
+                page.putField("answer1", content.getText());
+                WebElement title = driver.findElement(By.className("ask-title"));
+                page.putField("title1", title.getText());
+            }*/
+        } else {
             page.setSkip(true);
-            skipNum.addAndGet(1L);
         }
     }
 
@@ -40,18 +47,26 @@ public class BaiduZhidaoPageProcessor implements PageProcessor {
         return site;
     }
 
-    public long getAtomicLong(){
-        return atomicLong.get();
-    }
 
-    public long getSkipNum(){
-        return skipNum.get();
-    }
-
-    public static void main(String[] args) {
-        BaiduZhidaoPageProcessor baiduZhidao = new BaiduZhidaoPageProcessor();
+    public static void main(String[] args) throws IOException {
+        /*BaiduZhidaoPageProcessor baiduZhidao = new BaiduZhidaoPageProcessor();
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\Users\\pc\\Downloads\\chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+        driver = new RemoteWebDriver(service.getUrl(),
+                DesiredCapabilities.chrome());
         Spider.create(baiduZhidao).addUrl("https://zhidao.baidu.com/").thread(5).run();
+        service.stop();
+        driver.close();
         System.out.println(baiduZhidao.getAtomicLong());
-        System.out.println(baiduZhidao.getSkipNum());
+        System.out.println(baiduZhidao.getSkipNum());*/
+        Spider.create(new BaiduZhidaoPageProcessor()).thread(5)
+                //.setScheduler(new RedisScheduler("localhost"))
+                // .addPipeline(new FilePipeline("C:\\Users\\pc\\Downloads\\test"))
+                .setDownloader(new SeleniumDownloader("C:\\Users\\pc\\Downloads\\chromedriver.exe"))
+                .addUrl("https://zhidao.baidu.com/")
+                .run();
     }
 }
---
+++ crawler/src/main/java/me/akuo/sel/ChromeTest.java
@@ -0,0 +1,65 @@
+package me.akuo.sel;
+
+/**
+ * Created by Akuo on 2017/4/12.
+ */
+import static org.junit.Assert.assertEquals;
+
+import junit.framework.TestCase;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+@RunWith(JUnit4.class)
+public class ChromeTest extends TestCase {
+
+    private static ChromeDriverService service;
+    private WebDriver driver;
+
+     @BeforeClass
+    public static void createAndStartService() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\Users\\pc\\Downloads\\chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+     @AfterClass
+    public static void createAndStopService() {
+        service.stop();
+    }
+
+     @Before
+    public void createDriver() {
+        driver = new RemoteWebDriver(service.getUrl(),
+                DesiredCapabilities.chrome());
+    }
+
+     @After
+    public void quitDriver() {
+        driver.quit();
+    }
+
+     @Test
+    public void testGoogleSearch() throws Exception {
+        driver.get("https://zhidao.baidu.com/question/1993438489854455427.html");
+         WebElement bestDiv = driver.findElement(By.className("wgt-best"));
+         String id = bestDiv.getAttribute("id");
+         if(id != null && id.startsWith("best-answer-")){
+             WebElement content  = driver.findElement(By.id("best-content-"+ id.replace("best-answer-","")));
+             System.out.println(content.getText());
+         }
+         WebElement title = driver.findElement(By.className("ask-title"));
+        assertEquals("谢娜回应打压吴昕是怎么回事？", title.getText());
+    }
+}
