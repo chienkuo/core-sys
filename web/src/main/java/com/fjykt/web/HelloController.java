f2fe780c3a91360b26c7d94229df66c5ad307844
张建国 <>
2017-06-24 00:14:17 +0800

    remove unused import

--- web/src/main/java/com/fjykt/web/HelloController.java
+++ web/src/main/java/com/fjykt/web/HelloController.java
@@ -1,11 +1,9 @@
 package com.fjykt.web;
 
 import com.fjykt.service.HelloWorld;
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
